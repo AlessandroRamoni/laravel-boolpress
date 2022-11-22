1d02b3bb0449bf266d236fdf7c3638e544f55bd1
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-22 16:46:40 +0100

    add NavComponent

---
+++ resources/js/components/NavComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <nav>
+            <ul>
+                <!-- <li>
+                    <router-link to="/">Home</router-link>
+                    <router-link to="/about">Chi siamo</router-link>
+                    <router-link to="/contacts">Contatti</router-link>
+                </li> -->
+                <li v-for="link, index in nav" :key="index">
+                    <router-link :to="link.path">{{ link.label }}</router-link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'NavComponent',
+    props: {
+        nav: Array
+    },
+};
+</script>
+
+<style scoped lang="scss">
+ul {
+    list-style: none;
+    display: flex;
+
+    li {
+        padding-top: 10px;
+        padding-left: 10px;
+    }
+}
+</style>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,14 +1,33 @@
 <template>
     <div>
+        <NavComponent :nav="nav" />
         <router-view></router-view>
     </div>
 </template>
 
 <script>
 
+import NavComponent from '../components/NavComponent.vue'
 
 export default {
-    name: 'App'
+    name: 'App',
+    data() {
+        return {
+            nav: [{
+                path: '/',
+                label: 'Home'
+            }, {
+                path: '/about',
+                label: 'Chi siamo'
+            }, {
+                path: '/contacts',
+                label: 'Contatti'
+            }],
+        }
+    },
+    components: {
+        NavComponent
+    }
 };
 </script>
 
