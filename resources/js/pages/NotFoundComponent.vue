cbeb9eded83ae64988a33e50eae52ad38647ccb3
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-22 16:16:29 +0100

    create NotFoundComponent

---
+++ resources/js/pages/NotFoundComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Error 404: Not Found!</h2>
+        <router-link to="/">Go to home </router-link>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'NotFoundComponent'
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
